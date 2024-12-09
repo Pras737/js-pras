56faeb98b809d20d2a9856ba0ec1fec5d2501590
Prashant Kumar <95523695+Pras737@users.noreply.github.com>
2024-12-09 18:24:33 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId=145698
+let accountEmail="pras@gmail.com"
+var accountPass='123456'
+accountCity="varanasi"
+
+accountEmail='hdfc@gamil.com'
+// accountId=55555
+accountPass='789456'
+accountCity='bengaluru'
+
+console.table([accountEmail,accountId,accountPass,accountCity])
