0bcb515a0430451c1ab93b1b479c79dc649e3d36
Prashant Kumar <95523695+Pras737@users.noreply.github.com>
2024-12-13 18:52:54 +0000

    string

---
+++ 01_basics/05_string.js
@@ -0,0 +1,8 @@
+const name="prashant"
+const repocount=50
+// console.log(name+repocount+"value");
+console.log(`hello my name ${name} and my repo ${repocount}`);
+
+const gameName="shivangee"
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('e'));
